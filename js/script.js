e9b1a326d60c0cfd79f851bf80fd8a318dbe4cc1
ANAGH <anaghkrkkl@gmail.com>
2021-03-13 13:53:10 +0530

    problem solved

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-$("#submit-form").submit((e) => {
-    alert("Comment Senting to Sever")
-    e.preventDefault()
-    $.ajax({
-        url: "https://script.google.com/macros/s/AKfycbxvTPuNrEwBQXPbIe0tn6PUDmJdFC0f4Xv3ZpOOM5KNQrU6vJhSIaDT/exec",
-        data: $("#submit-form").serialize(),
-        method: "post",
-        success: function (response) {
-            window.location.href = "success.html"
-        },
-        error: function (err) {
-            alert("Something Error")
-
-        }
-    })
-})
 var TxtRotate = function(el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
